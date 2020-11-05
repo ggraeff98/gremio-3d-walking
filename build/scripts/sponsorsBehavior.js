bb35b31fbaf078699d5bc4d4072fca386df200f7
Gustavo <gustavograeff1998@gmail.com>
2020-11-05 12:40:07 -0300

    adiciona logo arena, museu, sua marca e muda posicao do logo do time

--- build/scripts/sponsorsBehavior.js
+++ build/scripts/sponsorsBehavior.js
@@ -37,26 +37,32 @@ const SPONSORS = [
   {
     company: "Umbro",
     logo: "../assets/logos/UMBRO.png",
-    link: "https://www.umbro.com.br/" 
+    link: "https://www.umbro.com.br/"
+  },
+  {
+    company: "SuaMarca",
+    logo: "../assets/logos/SUA-MARCA.png",
+    link: ""
   }
 ];
 
-const FIRST_SPONSOR_TO_SHOW = 0;
+const FIRST_SPONSOR_TO_SHOW_INDEX = 0;
 
 const LAST_SPONSOR_TO_SHOW_INDEX = SPONSORS.length - 1;
 
 let currentSponsor = LAST_SPONSOR_TO_SHOW_INDEX;
 
 const SPONSOR_INDEX = {
-  '/ambev': 0,
-  '/banrisul': 1,
-  '/betsul': 2,
-  '/brahma': 3,
-  '/chevrolet': 4,
-  '/panvel': 5,
-  '/safeweb': 6,
-  '/umbro': 7,
-}
+  "/ambev": 0,
+  "/banrisul": 1,
+  "/betsul": 2,
+  "/brahma": 3,
+  "/chevrolet": 4,
+  "/panvel": 5,
+  "/safeweb": 6,
+  "/umbro": 7,
+  "/suamarca": 8
+};
 
 function showSponsor(link, logo) {
   jQuery(".sponsors").find(".link").attr("href", link);
@@ -70,23 +76,23 @@ function showSponsor(link, logo) {
 }
 
 function changeShowingSponsor() {
-  if (document.location.pathname !== '/') {
-    setSponsorIndexToShow(document.location.pathname)
+  if (document.location.pathname !== "/") {
+    setSponsorIndexToShow(document.location.pathname);
   } else {
     if (currentSponsor === LAST_SPONSOR_TO_SHOW_INDEX) {
-      currentSponsor = FIRST_SPONSOR_TO_SHOW;
+      currentSponsor = FIRST_SPONSOR_TO_SHOW_INDEX;
     } else {
       currentSponsor++;
     }
   }
 
   setTimeout(() => {
-    const banrisulLogo = SPONSOR_INDEX['/banrisul'];
-    const adidasLogo = SPONSOR_INDEX['/umbro'];
+    const banrisulLogo = SPONSOR_INDEX["/banrisul"];
+    const umbroLogo = SPONSOR_INDEX["/umbro"];
 
     if (currentSponsor === banrisulLogo) {
       jQuery(".sponsors").find(".logo").addClass("banrisul-logo");
-    } else if (currentSponsor === adidasLogo) {
+    } else if (currentSponsor === umbroLogo) {
       jQuery(".sponsors").find(".logo").addClass("umbro-logo");
     } else {
       jQuery(".sponsors").find(".logo").removeClass("banrisul-logo");
